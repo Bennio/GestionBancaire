a88038d9ee18f1d30c049f5f7aa25b5f932129a8
Bendy Latortue <bendy.latortue@gmail.com>
2018-01-25 21:12:41 +0700

    Dash 1.0

---
+++ src/presentation/Dash.java
@@ -0,0 +1,14 @@
+package presentation;
+
+public class Dash {
+
+	public Dash() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
